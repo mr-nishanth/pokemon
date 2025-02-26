49575b3ad0782e76cbb5ddddd3a7fd0ab0b71ec1
mr-nishanth <developernishanth2000@gmail.com>
2025-02-27 01:31:35 +0530

    feat(apps): add toTitleCase function for string formatting in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,3 +6,15 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function toTitleCase(str: string): string {
+  if (typeof str !== 'string' || !str.trim()) {
+    return '';
+  }
+
+  return str
+    .replace(/[-_]/g, ' ')
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
